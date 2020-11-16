93d29f0c4ce6df0600cf868cb1a2db25d10995ed
Bislam S <44383332+dtf1337@users.noreply.github.com>
2020-11-16 09:30:27 -0700

    maybe final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, {Component} from 'react'
-import {Switch, Route} from 'react-router-dom'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar'
-import ProductList from './components/ProductList'
-import Details from './components/Details'
-import Default from './components/Default'
-import Cart from './components/Cart/Cart'
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default";
+import Modal from "./components/Modal";
 
-function App() {
-  return (
-   <>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={ProductList} />
-      <Route path="/details" component={Details} />
-      <Route path="/cart" component={Cart} />
-      <Route component={Default} />
-    </Switch>
-   </>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+        <Modal />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PayPalBtn from "./PayPalBtn";
+import { Link } from "react-router-dom";
+
+export default function CartTotals({ value, history }) {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={() => clearCart()}
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5>
+              <span className="text-title">subtotal: </span>
+              <strong>${cartSubTotal}</strong>
+            </h5>
+            <h5>
+              <span className="text-title">tax: </span>
+              <strong>${cartTax}</strong>
+            </h5>
+            <h5>
+              <span className="text-title"> cart total: </span>
+              <strong>${cartTotal}</strong>
+            </h5>
+            <PayPalBtn
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
