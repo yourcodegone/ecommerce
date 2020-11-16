37d8e19d1386c24b1a313ddca10a67482b1e8223
Bislam S <44383332+dtf1337@users.noreply.github.com>
2020-11-16 11:53:53 -0700

    j

--- src/components/Cart/PayPalBtn.js
+++ src/components/Cart/PayPalBtn.js
@@ -22,13 +22,13 @@ export default class MyApp extends React.Component {
       // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
     };
 
-    let env = "sandbox"; // you can set here to 'production' for production
+    let env = "sandbox"; // you can set here to 'productilon' for production
     let currency = "USD"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    // let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: "key",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
