48c527bad1cea67fa9936e007c6b4fc6c99eb350
Bislam S <44383332+dtf1337@users.noreply.github.com>
2020-11-16 11:33:08 -0700

    up

--- src/components/Cart/PayPalBtn.js
+++ src/components/Cart/PayPalBtn.js
@@ -29,7 +29,7 @@ export default class MyApp extends React.Component {
 
     const client = {
       sandbox: process.env.REACT_APP_APPID,
-      production: ""
+      production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
     // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
